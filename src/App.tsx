bd6b9af3fc051453f63ff9eb3a2d679afb455dae
João Bispo <joaobispo2077@gmail.com>
2021-05-29 17:50:13 -0300

    style: added css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,15 @@
 import React from 'react';
-import { PomodoroTimer } from './components/pomodoroTimer';
+import PomodoroTimer from './components/PomodoroTimer';
 
 function App(): JSX.Element {
   return (
-    <div className="App">
-      <PomodoroTimer defaultPomodoroTime={1500} />
+    <div className="container">
+      <PomodoroTimer
+        defaultPomodoroTime={1500}
+        shortRestTime={300}
+        longRestTime={900}
+        cycles={4}
+      />
     </div>
   );
 }
--- src/components/pomodoroTimer.tsx
+++ src/components/pomodoroTimer.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
-import Button from '../components/Button';
-import Timer from '../components/Timer';
+import Button from './Button';
+import Timer from './Timer';
 
 import { useInterval } from '../hooks/useInterval';
 interface IPomodoroTimerProps {
   defaultPomodoroTime: number;
+  shortRestTime: number;
+  longRestTime: number;
+  cycles: number;
 }
 
-export function PomodoroTimer(props: IPomodoroTimerProps): JSX.Element {
+function PomodoroTimer(props: IPomodoroTimerProps): JSX.Element {
   const [mainTime, setMainTime] = React.useState(props.defaultPomodoroTime);
 
   useInterval(() => {
@@ -18,11 +21,41 @@ export function PomodoroTimer(props: IPomodoroTimerProps): JSX.Element {
     <div id="pomodoro">
       <h2>You are working</h2>
       <Timer mainTime={mainTime} />
-      <Button
-        text="Start"
-        onClick={() => console.log('Hello')}
-        className="abc"
-      />
+
+      <div className="controls">
+        <Button
+          text="Start"
+          onClick={() => console.log('Hello')}
+          className="abc"
+        />
+        <Button
+          text="Start"
+          onClick={() => console.log('Hello')}
+          className="abc"
+        />
+        <Button
+          text="Start"
+          onClick={() => console.log('Hello')}
+          className="abc"
+        />
+      </div>
+
+      <div className="details">
+        <p>
+          Testando: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Testando: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Testando: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+        <p>
+          Testando: Lorem ipsum dolor sit amet consectetur adipisicing elit.
+        </p>
+      </div>
     </div>
   );
 }
+
+export default PomodoroTimer;
